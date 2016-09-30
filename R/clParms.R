9ef096e89f90d47ebddfb8349b54d112df4a04fc
Dan Murphy <danielmarkmurphy@gmail.com>
2016-09-30 12:11:10 -0700

    Calls NA sigma estimation functions

--- R/clParms.R
+++ R/clParms.R
@@ -1,23 +1,82 @@
-clParms <- function(clSoln) {
-  smmry <- suppressWarnings(lapply(clSoln$Models, summary))
+clParms <- function(clsoln, est.sigma = "linearcv") {
+  est.sigma <- match.arg(est.sigma, c(
+    "linearcv", "Mack", "log-linear"
+  ))
+  smmry <- suppressWarnings(lapply(clsoln$Models, summary))
   f <- sapply(smmry, function(x) x$coef["x","Estimate"])
   f.se <- sapply(smmry, function(x) x$coef["x","Std. Error"])
   sigma <- sapply(smmry, function(x) x$sigma)
-  df <- sapply(smmry, function(x) x$df[2L])
-  alpha <- clSoln$delta
   # Check for NA/NaN variance parameters
   w <- which(is.na(sigma))
   if (w) {
-    v <- mean(sigma[-w] / (f[-w] - 1))
-    sigma[w] <- v * (f[w] - 1)
+    sigma <- switch(est.sigma,
+                   linearcv = estimateSigmaLinearCV(sigma, f),
+                   Mack = estimateSigmaMack(sigma),
+                   `log-linear` = estimateSigmaLogLinear(sigma))
     f.se[w] <- sapply(w, function(s){
-      dat <- clSoln$Models[[s]]$model
+      dat <- clsoln$Models[[s]]$model
       sigma[s] / sqrt(sum(dat$x^(2-alpha[s])))
     })
   }
+  df <- sapply(smmry, function(x) x$df[2L])
+  alpha <- clsoln$delta
   data.frame(f, f.se, sigma, df, alpha)
 }
 fminus1.cv <- function(macksoln) {
   x <- mackParms(macksoln)
   x$f.se / (x$f - 1)
+}
+clParmsLogLinear <- function(clsoln) {
+  smmry <- suppressWarnings(lapply(clsoln$Models, summary))
+  f <- sapply(smmry, function(x) x$coef["x","Estimate"])
+  f.se <- sapply(smmry, function(x) x$coef["x","Std. Error"])
+  sigma <- sapply(smmry, function(x) x$sigma)
+  # Check for NA/NaN variance parameter
+  w <- which(is.na(sigma))
+  if (w) {
+    sigma <- estimateSigmaLogLinear(sigma)
+    f.se[w] <- sapply(w, function(s){
+      dat <- clsoln$Models[[s]]$model
+      sigma[s] / sqrt(sum(dat$x^(2-alpha[s])))
+    })
+  }
+  df <- sapply(smmry, function(x) x$df[2L])
+  alpha <- clsoln$delta
+  data.frame(f, f.se, sigma, df, alpha)
+}
+clParmsMack <- function(clsoln) {
+  smmry <- suppressWarnings(lapply(clsoln$Models, summary))
+  f <- sapply(smmry, function(x) x$coef["x","Estimate"])
+  f.se <- sapply(smmry, function(x) x$coef["x","Std. Error"])
+  sigma <- sapply(smmry, function(x) x$sigma)
+  # Check for NA/NaN variance parameter
+  w <- which(is.na(sigma))
+  if (w) {
+    sigma <- estimateSigmaMack(sigma)
+    f.se[w] <- sapply(w, function(s) {
+      dat <- clsoln$Models[[s]]$model
+      sigma[s] / sqrt(sum(dat$x^(2-alpha[s])))
+    })
+  }
+  df <- sapply(smmry, function(x) x$df[2L])
+  alpha <- clsoln$delta
+  data.frame(f, f.se, sigma, df, alpha)
+}
+clParmsLinearCV <- function(clsoln) {
+  smmry <- suppressWarnings(lapply(clsoln$Models, summary))
+  f <- sapply(smmry, function(x) x$coef["x","Estimate"])
+  f.se <- sapply(smmry, function(x) x$coef["x","Std. Error"])
+  sigma <- sapply(smmry, function(x) x$sigma)
+  # Check for NA/NaN variance parameter
+  w <- which(is.na(sigma))
+  if (w) {
+    sigma <- estimateSigmaLinearCV(sigma, f)
+    f.se[w] <- sapply(w, function(s) {
+      dat <- clsoln$Models[[s]]$model
+      sigma[s] / sqrt(sum(dat$x^(2-alpha[s])))
+    })
+  }
+  df <- sapply(smmry, function(x) x$df[2L])
+  alpha <- clsoln$delta
+  data.frame(f, f.se, sigma, df, alpha)
 }
