24edca482fb0cec8fd6e4b44fcc0a8fec3d47949
Dan Murphy <danielmarkmurphy@gmail.com>
2016-09-30 12:08:44 -0700

    Edits and new summary functions etc

--- R/futureTriangles.R
+++ R/futureTriangles.R
@@ -1,7 +1,7 @@
 #   futureTriangle.expd
 mostCurrentDiagonal <- ChainLadder::getLatestCumulative
 futureTriangles <- function(Triangle, clparms, 
-                                 tail = 1.0, tail.se = 0, tail.sigma = 0, tail.alpha = 1) {
+                                 tail = 1.0, tail.se = 0, tail.sigma = 0, tail.alpha) {
   f <- clparms$f
   f.se <- clparms$f.se
   sigma <- clparms$sigma
@@ -12,6 +12,7 @@ futureTriangles <- function(Triangle, clparms,
     f     <- c(f,     tail)
     f.se  <- c(f.se,  tail.se)
     sigma <- c(sigma, tail.sigma)
+    if (missing(tail.alpha)) tail.alpha <- tail(alpha, 1)
     alpha <- c(alpha, tail.alpha)
     df 	  <- c(df,    NA)
   }
@@ -68,24 +69,120 @@ futureTriangles <- function(Triangle, clparms,
   mcdsourceage <- attr(mcd, "colnames")
   B <- sapply(sourcenms, function(s) sum(mcd[mcdsourceage == s]))
 	# Then ..
-  TotalAmtDeveloped <- cumsum(c(B, 0) + colSums(ft, na.rm = TRUE))
+  TotalAmtDeveloped <- c(B, 0) + colSums(ft, na.rm = TRUE)
   
 	# Using TotalAmtDeveloped, run through same formulas as above.
   for (i in 1:n) {
     srce <- sourcenms[i]
     target <- targetnms[i]
-    FT.d[target] <- (TotalAmtDeveloped[i])^2 * f.se2[target] + f2[target] * FT.d[srce] +
+    FT.d[target] <- (TotalAmtDeveloped[srce])^2 * f.se2[target] + f2[target] * FT.d[srce] +
       f.se2[target] * FT.d[srce]
     FT.g[target]  <- #expectedValueXtoTheAlpha(ft[i, srce], alpha[target]) * sigma
-      (TotalAmtDeveloped[i]^alpha[target] + 
+      (TotalAmtDeveloped[srce]^alpha[target] + 
          .5 * alpha[target] * (alpha[target]-1) * 
-          TotalAmtDeveloped[i]^(alpha[target] - 2) * FT.g[srce]) * sigma2[target] + 
+          TotalAmtDeveloped[srce]^(alpha[target] - 2) * FT.g[srce]) * sigma2[target] + 
           f2[target] * FT.g[srce]
   }
   
-  list(ft = ft, ft.d = sqrt(ft.d), ft.g = sqrt(ft.g), 
-       FT = FT, FT.d = sqrt(FT.d), FT.g = sqrt(FT.g),
-       clfmparms = data.frame(f = f, f.se = f.se, sigma = sigma, 
-														  df = df, alpha = alpha), 
-			 source = sourcenms, target = targetnms)
+  structure(
+    list(ft = ft, ft.d = sqrt(ft.d), ft.g = sqrt(ft.g), 
+         FT = FT, FT.d = sqrt(FT.d), FT.g = sqrt(FT.g),
+         mcd = mcd,
+         clfmparms = data.frame(f = f, f.se = f.se, sigma = sigma, 
+				  										  df = df, alpha = alpha), 
+         tail = tail, tail.se = tail.se, tail.sigma = tail.sigma, tail.alpha = tail.alpha,
+			   source = sourcenms, target = targetnms),
+    class = "clfm")
+}
+summary.clfm <- function(clfmsoln) {
+  exh <- data.frame(
+    origin = names(clfmsoln$mcd),
+    latest = clfmsoln$mcd,
+    ldf = cumprod(rev(clfmsoln$clfmparms$f)),
+    ultimate = clfmsoln$ft[, "Inf"],
+    se = sqrt(clfmsoln$ft.d[, "Inf"]^2 + clfmsoln$ft.g[, "Inf"]^2)
+  )
+  exh <- rbind(exh, data.frame(
+    origin = "Sum",
+    latest = sum(clfmsoln$mcd),
+    ldf = as.numeric(NA),
+    ultimate = sum(clfmsoln$ft[, "Inf"]),
+    se = sqrt(clfmsoln$FT.d["Inf"]^2 + clfmsoln$FT.g["Inf"]^2)
+    ))
+  names(exh)[1L] <- attr(clfmsoln$mcd, "rowsname")
+  exh
+}
+summaryMack <- function(x, ...) {UseMethod("summaryMack")}
+summaryMack.MackChainLadder <- ChainLadder:::summary.MackChainLadder
+summaryMack.clfm <- function(x, ...){
+  
+  ## Summarise my results
+  Latest <- x$mcd
+  
+  ex.origin.period <- Latest!=0
+  
+  Ultimate <- x$ft[, "Inf"]
+  Dev.To.Date <- Latest/Ultimate
+  IBNR <- Ultimate-Latest
+  Mack.S.E <- sqrt(x$ft.d[, "Inf"]^2 + x$ft.g[, "Inf"]^2)
+  CV <- Mack.S.E/(Ultimate-Latest)
+  
+  ByOrigin <- data.frame(Latest, Dev.To.Date, Ultimate, IBNR, Mack.S.E, CV)
+  names(ByOrigin)[6]="CV(IBNR)"
+  ByOrigin <- ByOrigin[ex.origin.period,]
+  
+  Totals <-  c(sum(Latest,na.rm=TRUE),
+               sum(Latest,na.rm=TRUE)/sum(Ultimate,na.rm=TRUE),
+               sum(Ultimate,na.rm=TRUE),
+               sum(IBNR,na.rm=TRUE), sqrt(x$FT.d["Inf"]^2 + x$FT.g["Inf"]^2),
+               sqrt(x$FT.d["Inf"]^2 + x$FT.g["Inf"]^2)/sum(IBNR,na.rm=TRUE)
+  )
+  # Totals <- c(Totals, round(x[["Total.Mack.S.E"]]/sum(res$IBNR,na.rm=TRUE),2))
+  Totals <- as.data.frame(Totals)
+  
+  colnames(Totals)=c("Totals")
+  rownames(Totals) <- c("Latest:","Dev:","Ultimate:",
+                        "IBNR:","Mack S.E.:",
+                        "CV(IBNR):")
+  
+  output <- list(ByOrigin=ByOrigin, Totals=Totals)
+  return(output)
+}
+print.clfm <- function(x, ...){
+  
+  ## Summarise my results
+  Latest <- x$mcd
+  
+  ex.origin.period <- Latest!=0
+  
+  Ultimate <- x$ft[, "Inf"]
+  Dev.To.Date <- Latest/Ultimate
+  IBNR <- Ultimate-Latest
+  Mack.S.E <- sqrt(x$ft.d[, "Inf"]^2 + x$ft.g[, "Inf"]^2)
+  CV <- Mack.S.E/(Ultimate-Latest)
+  
+  ByOrigin <- data.frame(Latest, Dev.To.Date, Ultimate, IBNR, Mack.S.E, CV)
+  names(ByOrigin)[6]="CV(IBNR)"
+  ByOrigin <- ByOrigin[ex.origin.period,]
+  print(format(ByOrigin, big.mark = ",", digits = 3),...)
+  
+  Totals <-  c(sum(Latest,na.rm=TRUE),
+               sum(Latest,na.rm=TRUE)/sum(Ultimate,na.rm=TRUE),
+               sum(Ultimate,na.rm=TRUE),
+               sum(IBNR,na.rm=TRUE), sqrt(x$FT.d["Inf"]^2 + x$FT.g["Inf"]^2),
+               sqrt(x$FT.d["Inf"]^2 + x$FT.g["Inf"]^2)/sum(IBNR,na.rm=TRUE)
+  )
+  # Totals <- c(Totals, round(x[["Total.Mack.S.E"]]/sum(res$IBNR,na.rm=TRUE),2))
+  Totals <- as.data.frame(Totals)
+  
+  colnames(Totals)=c("Totals")
+  rownames(Totals) <- c("Latest:","Dev:","Ultimate:",
+                        "IBNR:","Mack S.E.:",
+                        "CV(IBNR):")
+  Totals[1:6,] <- formatC(Totals[1:6,], big.mark=",",digits=2,format="f")
+  cat("\n")
+  print(Totals, quote=FALSE)
+#  
+#  output <- list(ByOrigin=ByOrigin, Totals=Totals)
+#  return(output)
 }
